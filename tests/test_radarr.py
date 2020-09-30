5e65e8812932e1b1cc6b418cea57cbda3e6abc31
marksie1988 <marksie1988@users.noreply.github.com>
2020-09-30 09:23:42 +0100

    Start writing tests

---
+++ tests/test_radarr.py
@@ -0,0 +1,10 @@
+# Standard library imports...
+from unittest.mock import Mock, patch
+
+# Third-party imports...
+from nose.tools import assert_is_not_none
+
+# Local imports...
+from PyArr import RadarrAPI
+
+def test_
